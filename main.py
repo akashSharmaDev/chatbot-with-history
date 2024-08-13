3ce4bb7b4b8fad59c810c03f3adf465e08d0211f
akashSharmaDev <imakashsharma135@gmail.com>
2024-08-13 22:28:58 +0530

    Fixed message history not loading logic

--- main.py
+++ main.py
@@ -45,7 +45,10 @@ chain = prompt | model
 store = {}
 
 def get_session_history(session_id: str) -> BaseChatMessageHistory:
-    if session_id is not None:
+    """
+        Loads Messages of older chats
+    """
+    if session_id not in store:
         store[session_id] = InMemoryChatMessageHistory()
     return store[session_id]
 
